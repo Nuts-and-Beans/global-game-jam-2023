2fe981800e8c4fb89db225db43ffd31655c2ebd9
William Whitehouse <williamswhitehouse@gmail.com>
2023-02-04 11:43:30 +0000

    Worked on Grid and Grid Texture/Camera

---
+++ UnityProj/Assets/Scripts/GridTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridTexture : MonoBehaviour
+{
+    [SerializeField] private RawImage _rawImage;
+    
+    private void Start()
+    {
+        _rawImage.texture = Grid.s_CameraRT;
+    }
+}
