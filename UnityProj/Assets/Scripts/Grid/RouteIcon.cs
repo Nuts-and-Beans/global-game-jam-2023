54a4e95fa012ff835f6e938155a3c5292cfdfe00
William Whitehouse <williamswhitehouse@gmail.com>
2023-02-04 15:06:21 +0000

    Set up route visualisation

---
+++ UnityProj/Assets/Scripts/Grid/RouteIcon.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+public class RouteIcon : MonoBehaviour
+{
+    public void SetPosition(float3 position)
+    {
+        transform.position = position;
+    }
+    
+    public void Enable()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
