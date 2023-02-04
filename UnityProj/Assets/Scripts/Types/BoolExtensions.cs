e4221377e70a499008711f9133c0019c08d8e67a
William Whitehouse <williamswhitehouse@gmail.com>
2023-02-04 14:13:09 +0000

    Worked on Grid Adventurer movement

---
+++ UnityProj/Assets/Scripts/Types/BoolExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Mathematics;
+
+public static class BoolExtensions
+{
+    public static bool Any(this bool2 b)
+    {
+        return b.x || b.y;
+    }
+    
+    public static bool All(this bool2 b)
+    {
+        return b.x && b.y;
+    }
+}
