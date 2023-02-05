afe58f8cdbf3de5d4a28def7a208430bbf402d86
j22-robertson <55785515+j22-robertson@users.noreply.github.com>
2023-02-05 11:04:42 +0000

    Made main menu look nicer

--- UnityProj/Assets/Scripts/MainMenuScript.cs
+++ UnityProj/Assets/Scripts/MainMenuScript.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] private Button _playButton;
+    [SerializeField] private TMPro.TMP_Text _playText;
+    [SerializeField] private TMPro.TMP_Text _exitText;
 
+    [SerializeField] private Button _exitButton;
     int selected = 0;
     [SerializeField] int num_buttons;
     private void Awake()
@@ -18,26 +24,34 @@ public class MainMenuScript : MonoBehaviour
 
     public void PlayGame()
     {
+        
         SceneManager.LoadScene("SampleScene");
     }
 
     public void OptionDown(InputAction.CallbackContext callback)
     {
+        int prev = selected;
+        revert(prev);
         selected++;
+        
         if(selected >= num_buttons )
         {
             selected =0;
         }
-        print(selected);
+        highlight(selected);
+       
     }
     public void OptionUp(InputAction.CallbackContext callback)
     {
+        int prev = selected;
+        revert(prev);
         selected--;
         if(selected < 0)
         {
             selected = num_buttons - 1 ;
         }
-        print(selected);
+        highlight(selected);
+       
     }
 
     public void SelectOption(InputAction.CallbackContext callback)
@@ -57,6 +71,33 @@ public class MainMenuScript : MonoBehaviour
          
         }
 
+    }
+    private void revert(int previous)
+    {
+        if(previous == 0)
+         {
+            _playText.text = "Play";
+            _playText.fontStyle = TMPro.FontStyles.Normal;
+        }
+        else if (previous == 1)
+        {
+            _exitText.text = "Exit";
+            _exitText.fontStyle = TMPro.FontStyles.Normal;
+        }
+    }
+    private void highlight(int selected)
+    {
+        if (selected == 0)
+        {
+            _playText.text = ">Play<";
+            _playText.fontStyle = TMPro.FontStyles.Underline
+                ;
+        }
+        else if (selected == 1)
+        {
+            _exitText.text = ">Exit<";
+            _exitText.fontStyle = TMPro.FontStyles.Underline;
+        }
     }
 
     public void ExitGame()
