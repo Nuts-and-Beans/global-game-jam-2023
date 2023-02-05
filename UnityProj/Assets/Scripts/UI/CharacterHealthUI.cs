242bc28cb264d09f387bf40df8e3df5315e33f53
Seb <Sebadam2010@hotmail.co.uk>
2023-02-05 11:54:25 +0000

    Minor CharacterHealthUI script change

--- UnityProj/Assets/Scripts/UI/CharacterHealthUI.cs
+++ UnityProj/Assets/Scripts/UI/CharacterHealthUI.cs
@@ -7,21 +7,12 @@ using UnityEngine.UI;
 
 public class CharacterHealthUI : MonoBehaviour
 {
-    [SerializeField] private GameObject _characterGameObject;
-    
     [SerializeField] private Image _heartFullImage;
     [SerializeField] private Image _heartEmptyImage;
 
-    private CharacterObject _characterObject;
+    [SerializeField] private CharacterObject _characterObject;
     
     [SerializeField] private Image[] _hearts;
-
-    private void Awake()
-    {
-        //NOTE(Sebadam2010): This is here for testing mainly.
-        //Ideally, would have the script responsible for putting the character on the tab into the map do the setting of the character.
-        setCharacter(_characterGameObject);
-    }
 
     private void Start()
     {
@@ -32,12 +23,6 @@ public class CharacterHealthUI : MonoBehaviour
     private void OnDestroy()
     {
         _characterObject._character.OnCharacterHealthEvent -= UpdateHeartAmount;
-    }
-    
-    public void setCharacter(GameObject character)
-    {
-        _characterGameObject = character;
-        _characterObject = _characterGameObject.GetComponent<CharacterObject>();
     }
 
     private void UpdateHeartAmount(int characterHealth)
