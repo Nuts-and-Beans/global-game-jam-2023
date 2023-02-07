17909ab80735de761655efc426042a1a7f792ed2
William Whitehouse <williamswhitehouse@gmail.com>
2023-02-07 10:20:36 +0000

    Updated route icon to have an anim. Updated encounter icon Particle System

--- UnityProj/Assets/Scripts/Grid/RouteIcon.cs
+++ UnityProj/Assets/Scripts/Grid/RouteIcon.cs
@@ -1,8 +1,17 @@
-﻿using Unity.Mathematics;
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class RouteIcon : MonoBehaviour
 {
+    [Header("Scene References")]
+    [SerializeField] private Canvas _imageCanvas;
+    [SerializeField] private float3 _startScale;
+    [SerializeField] private float3 _endScale;
+    [SerializeField] private float _duration;
+
     public void SetPosition(float3 position)
     {
         transform.position = position;
@@ -11,10 +20,36 @@ public class RouteIcon : MonoBehaviour
     public void Enable()
     {
         gameObject.SetActive(true);
+        StartCoroutine(DoLerp());
     }
 
     public void Disable()
     {
         gameObject.SetActive(false);
     }
+    
+    private IEnumerator DoLerp()
+    {
+        yield return Lerp(_startScale, _endScale);
+        yield return Lerp(_endScale, _startScale);
+    }
+    
+    private IEnumerator Lerp(float3 start, float3 end)
+    {
+        float timeElapsed  = 0.0f;
+        
+        while (timeElapsed < _duration)
+        {
+            float t = Sinusoidal(timeElapsed / _duration);
+            _imageCanvas.transform.localScale = Vector3.Lerp(start, end, t);
+            
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    
+        _imageCanvas.transform.localScale = end;
+    }
+    
+    [BurstCompile, MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Sinusoidal(float val) => math.sin(val * (math.PI * 0.5f));
 }
